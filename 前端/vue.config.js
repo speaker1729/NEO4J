e89f164574a97b6c42117ca48928837f32292ccf
speaker1729 <126320604+speaker1729@users.noreply.github.com>
2024-05-09 21:29:34 +0800

    Delete 前端 directory

--- 前端/vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-
-module.exports = defineConfig({
-  publicPath: './',
-  transpileDependencies: true,
-  devServer: {
-    proxy: 'http://localhost:9093'
-  }
-})
